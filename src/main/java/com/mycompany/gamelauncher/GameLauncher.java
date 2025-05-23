f7bbad78d8cc950fc114d96702b0fb74ad54b06c
wolftxt <wolfdavid49@gmail.com>
2025-05-23 10:13:25 +0200

    Initialised Maven project

---
+++ src/main/java/com/mycompany/gamelauncher/GameLauncher.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gamelauncher;
+
+/**
+ *
+ * @author davidwolf
+ */
+public class GameLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
